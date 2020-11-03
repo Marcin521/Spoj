05ae6b1356a448b789a83d71edef74c94e598bde
Marcin521 <marcinkondo134@gmail.com>
2020-11-03 14:09:55 +0100

    write sum recursively

--- suma/main.cpp
+++ suma/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int f(int n)
+{
+    if(n==0) return 0;
+    else return f(n-1)+n;
+}
+
+int main()
+{
+    cout << "wynik " << f(0) << endl;
+
     return 0;
 }
