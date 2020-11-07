c29806fd84143bcefcf8ae67ec32b9e03cb386ce
Marcin521 <marcinkondo134@gmail.com>
2020-11-07 08:50:39 +0100

    add loading numbers

--- exponentiation/main.cpp
+++ exponentiation/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
+
+int exponentiation(int a, int b)
+{
+    int result;
+    result = pow(a, b);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int n;
+
+    cout << "number of tries" << endl;
+    cin >> n;
+
+    int a, b, arr[n], arr2[n];
+
+    for( ; n>0; n--)
+    {
+        cin >> a >> b;
+        arr[n-1] = a;
+        arr2[n-1] = b;
+    }
+
     return 0;
 }
